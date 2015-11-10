1279db0bc53b7bc3f1dc8dca0f999743dffb9dc5
Joseph Gabriell Palmaras <josephpalmaras@gmail.com>
2015-11-10 13:23:34 +0800

    Game2updates

--- Unity Projects/New Unity Project 1/Assets/Scripts/NumberPickup.cs
+++ Unity Projects/New Unity Project 1/Assets/Scripts/NumberPickup.cs
@@ -3,12 +3,15 @@ using System.Collections;
 
 public class NumberPickup : MonoBehaviour {
 
-	public GameObject numberParticle;
-	
+	public GameObject numberParticle, number, ground;
+
 	void OnTriggerEnter2D (Collider2D other)
 	{
+		float posx = gameObject.transform.position.x + 15.0f;
 		if (other.GetComponent<PlayerController> () == null)
 			return;
+		Instantiate (number, new Vector3(posx, 0,0), gameObject.transform.rotation);
+		Instantiate (ground, new Vector3(posx, (-1.432954f),0), gameObject.transform.rotation);
 		Instantiate (numberParticle, gameObject.transform.position, gameObject.transform.rotation);
 		TextColor.changeColor(gameObject.GetComponent<SpriteRenderer> ().sprite.name);
 		Destroy (gameObject);

8a3afb8a0fa7f67e4ab86f8ab99e7899f3a05a1c
trishalim <limtrishajean@gmail.com>
2015-12-31 10:16:47 +0800

    login

---
+++ Unity Projects/Monitoring/Assets/SignInListener.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SignInListener : MonoBehaviour {
+
+	public InputField username;
+	public InputField password;
+
+	void Start () {
+		GetComponent<Button>().onClick.AddListener(() => Click());
+	}
+
+	void Click(){
+		WWWForm form = new WWWForm();
+		form.AddField("user", username.text);
+		form.AddField("pw", password.text);
+		WWW w = new WWW("http://localhost/PreschoolMath/login.php", form);
+		StartCoroutine (ClickListener(w));
+	}
+
+	IEnumerator ClickListener (WWW w){
+		yield return w;
+		Debug.Log (w.text);
+	}
+}

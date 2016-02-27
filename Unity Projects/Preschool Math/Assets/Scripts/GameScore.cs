098e6567c8905d69ca2685b85ef74952b55f81fd
trishalim <limtrishajean@gmail.com>
2016-02-27 12:26:43 +0800

    add score script

---
+++ Unity Projects/Preschool Math/Assets/Scripts/GameScore.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameScore : MonoBehaviour {
+
+	void Start() {
+		Debug.Log ("start");
+		//addScore (1, 1, 100);
+	}
+
+	public void addScore(int game_id, int level, int score) {
+		WWWForm form = new WWWForm();
+		form.AddField("game_id", game_id);
+		form.AddField("level", level);
+		form.AddField("score", score);
+		form.AddField("username", UserController.username);
+		WWW w = new WWW("http://preschoolmath.x10host.com/unity/add_score.php", form);
+		StartCoroutine (Call (w));
+	}
+
+	IEnumerator Call (WWW w){
+		yield return w;
+		Debug.Log ("call");
+		Debug.Log (w.text);
+	}
+}
